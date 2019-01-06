8f6ca7152950690ee56db72a68705e80acc95376
Strangely Twisted <thespecialist47@gmail.com>
2019-01-06 19:45:01 +0100

    Initial Commit

---
+++ Storyboards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storyboards
+//
+//  Created by Kornel Kwiecinski on 30/12/2018.
+//  Copyright © 2018 Kornel Kwiecinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+
+}
+
